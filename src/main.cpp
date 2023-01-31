3816d308e5bc72d569aac828d6406046fbee381b
Michele Pagot <michele.pagot@suse.com>
2023-01-31 14:33:08 +0100

    Add MESH setup, logs and proper board definition

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,69 @@
 #include <painlessMesh.h>
 
-#define   MESH_SSID       "whateverYouLike"
-#define   MESH_PASSWORD   "somethingSneaky"
+#define   MESH_SSID       "test1"
+#define   MESH_PASSWORD   "meshlongpassword"
 #define   MESH_PORT       5555
+
 
 Scheduler     userScheduler; // to control your personal task
 painlessMesh  mesh;
+SimpleList<uint32_t> nodes;
+
+
+void cb_NewConnection(uint32_t nodeId);
+void cb_ChangedConnections(); 
+void cb_Receive(uint32_t from, String & msg);
+void cb_NodeTimeAdjusted(int32_t offset); 
+void cb_NodeDelayReceived(uint32_t from, int32_t delay);
 
 void setup()
 {
+  Serial.begin(115200);
   mesh.setDebugMsgTypes(ERROR | DEBUG);  // set before init() so that you can see error messages
 
   mesh.init(MESH_SSID, MESH_PASSWORD, &userScheduler, MESH_PORT);
+
+  mesh.onNewConnection(&cb_NewConnection);
+  mesh.onChangedConnections(&cb_ChangedConnections);
+  mesh.onReceive(&cb_Receive);
+  mesh.onNodeTimeAdjusted(&cb_NodeTimeAdjusted);
+  mesh.onNodeDelayReceived(&cb_NodeDelayReceived);
+  Serial.print("Init DONE!!\r\n");
 }
 
 void loop()
 {
+  mesh.update();
 }
+
+void cb_NewConnection(uint32_t nodeId) {
+  Serial.printf("[NewConnection] nodeId = %u\r\n", nodeId);
+  Serial.printf("[NewConnection] %s\r\n", mesh.subConnectionJson(true).c_str());
+}
+
+void cb_ChangedConnections() {
+  nodes = mesh.getNodeList();
+
+  Serial.printf("[ChangedConnections] Num nodes: %d\r\n", nodes.size());
+  Serial.print("[ChangedConnections] Connection list:\r\n");
+
+  SimpleList<uint32_t>::iterator node = nodes.begin();
+  while (node != nodes.end()) {
+    Serial.printf("[ChangedConnections] %u\r\n", *node);
+    node++;
+  }
+}
+
+void cb_Receive(uint32_t from, String & msg) {
+  Serial.printf("[Receive] from %u msg=%s\r\n", from, msg.c_str());
+}
+
+void cb_NodeTimeAdjusted(int32_t offset) {
+  Serial.printf("[NodeTimeAdjusted] Adjusted time %u. Offset = %d\r\n", mesh.getNodeTime(), offset);
+}
+
+void cb_NodeDelayReceived(uint32_t from, int32_t delay) {
+  Serial.printf("[NodeDelayReceived] Delay to node %u is %d us\r\n", from, delay);
+}
+
 
